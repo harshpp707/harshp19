5c59d2cfcaa71755f80ceeb5abfafff19d3fb535
harshpp707 <73919744+harshpp707@users.noreply.github.com>
2022-05-18 11:33:03 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
+
+
+function wildfire(){
+    window.open("https://github.com/harshpp707/wildfire-prediction", "_blank")
+}
+
+function qr_code(){
+    window.open("https://github.com/CMPUT301W22T23/QR-Code-Game", "_blank")
+}
+
+function linkedin(){
+    window.open("https://www.linkedin.com/in/harsh-patel-528804202/", "_blank")
+}
